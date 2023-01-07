ba7b2a7d0a81ec5d6a8a8b283e0699e623a65af7
kcgroulx <87883655+kcgroulx@users.noreply.github.com>
2023-01-06 21:37:17 -0800

    Works! :D

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -2,6 +2,10 @@
 #Created by Kyle Groulx, Jan 2nd, 2023.
 #A Python script take user input for an unsolved Sudoku puzzle and prints the solved puzzle.
 
+
+import math
+import time
+
 #Checks for Sudoku Violation in array, ignoring zero.
 def hasDuplicates(array, row, col):
     num = array[row][col]
@@ -9,16 +13,22 @@ def hasDuplicates(array, row, col):
         return False
     rowArray = array[row]
     colArray = [rows[col] for rows in array]
-    return rowArray.count(num) > 1 or colArray.count(num) > 1
+    length = int(math.sqrt(len(array[0])))
+    squareRow = int((row // length) * length)
+    squareCol = int((col // length) * length)
+    squareArray = []
+    for row in range(squareRow, squareRow + length):
+        for col in range(squareCol, squareCol + length):
+            squareArray.append(array[row][col])
+
+    return rowArray.count(num) > 1 or colArray.count(num) > 1 or squareArray.count(num) > 1
 
 
 def solveSudoku(array):
     size = len(array[0])
-
     #if there are no 0s, the array is solved and returned
     if sum(x.count(0) for x in array) == 0:
         return array
-
     #interates through for a 0.
     for row in range(size):
         for col in range(size):
@@ -33,23 +43,69 @@ def solveSudoku(array):
                 return False    
 
 
-
-    
-
 #Gets 2D array Input from user and saves it into 2D array, numbers.
-inputString = input("Enter 9x9 Sudoku puzzle: \n")
-rows = inputString.split(',')
-numbers = [row.split() for row in rows] 
-numbers = [[int(x) for x in row] for row in numbers]
+"""
+numbers = [[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+"""
+numbers = [[8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
+"""
+numbers = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],]
+"""
+start = time.perf_counter()
 solvedSudoku = solveSudoku(numbers)
-for row in solvedSudoku:
-    print(row)
-            
-
-
-
-
-
-
-
+end = time.perf_counter()
+if solvedSudoku != False: 
+    print("\nSolution:")
+    for row in solvedSudoku:
+        print(row)
+    print(f"\nTime elapsed: {end - start:0.6f} seconds")
+else:
+    print("Unable to find Solution")
